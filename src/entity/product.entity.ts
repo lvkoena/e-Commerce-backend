0bb19827e9647d20619cff3b548c095f035e0ef4
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-06-04 13:21:07 +0200

    created entity file

---
+++ src/entity/product.entity.ts
@@ -0,0 +1,4 @@
+// import { Entity } from 'typeorm';
+
+// @Entity('Product')
+export class Product {}
--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from 'src/controllers/product/product.controller';
+import { Product } from 'src/entity/product.entity';
 import { ProductService } from 'src/services/product/product.service';
+// import { TypeOrmModule } from 'typeorm';
+
 
 @Module({
     // imports: [TypeOrmModule.forFeature([Product])],
