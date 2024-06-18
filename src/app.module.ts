e1bfd8e56c7140eefbcc7f6c56d7482be1dffc32
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-06-18 09:31:23 +0200

    updated products filename in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductController } from './controllers/products/products.controller';
-import { ProductModule } from './modules/products/products.module';
+import { ProductsController } from './controllers/products/products.controller';
+import { ProductsModule } from './modules/products/products.module';
 import { ProductService } from './services/products/products.service';
 
 @Module({
-  imports: [ProductModule],
-  controllers: [AppController, ProductController],
+  imports: [ProductsModule],
+  controllers: [AppController, ProductsController],
   providers: [AppService, ProductService],
 })
 export class AppModule {}
--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ProductsController } from 'src/controllers/products/products.controller';
+import { Products } from 'src/entity/products.entity';
 import { ProductService } from 'src/services/products/products.service';
 // import { TypeOrmModule } from 'typeorm';
 
 
 @Module({
-    // imports: [TypeOrmModule.forFeature([Product])],
+    // imports: [TypeOrmModule.forFeature([Products])],
     controllers: [ProductsController],
     providers: [ProductService],
     exports: [ProductService],
 })
-export class ProductModule {}
+export class ProductsModule {}
