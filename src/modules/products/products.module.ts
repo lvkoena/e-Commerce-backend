23eb1f6c01ec852ca6c7e00a3980bf34dfca3ed2
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-06-18 15:22:42 +0200

    updated filename in services

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from 'src/controllers/products/products.controller';
-import { Products } from 'src/entity/product.entity';
 import { ProductsService } from 'src/services/products/products.service';
 // import { TypeOrmModule } from 'typeorm';
 
