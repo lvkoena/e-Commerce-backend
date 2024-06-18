01572e5a4751d4e6f902982e9d2759aac797d49f
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-06-18 15:15:32 +0200

    created the updated method for crud operation

--- src/services/products/products.service.ts
+++ src/services/products/products.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import path from 'path';
 import { Product } from 'src/entity/product.entity';
 import { Repository } from 'typeorm';
+import { UpdateResult } from 'typeorm/driver/mongodb/typings';
 
 @Injectable()
 export class ProductsService {
     constructor(
         //Decorator function
         @InjectRepository(Product)
-        private readonly productsRepository: Repository<Product>,
-    ) {}
+        private readonly productsRepository: Repository<Product>,) {}
 
     // async create(createProductDto: CreateProductDto): Promise<Product> {
     //     return await this.productsRepository.save(createProductDto);
@@ -18,4 +19,12 @@ export class ProductsService {
     async findAll(): Promise<Product[]> {
         return await this.productsRepository.find();
     }
+
+    // async findOne(id: string): Promise<Product> {
+    //     return await this.productsRepository.findOne(id);
+    // }
+
+    // async update(id: string, updateProductDto: UpdateProductDto,): Promise<UpdateResult> {
+    //     return await this.productsRepository.update(id, updateProductDto);
+    // }
 }
