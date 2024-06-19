9a86ba2876ef03ac0da8a34246ba866aaec86a98
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-06-19 14:13:53 +0200

    updated entity file

--- src/entity/product.entity.ts
+++ src/entity/product.entity.ts
@@ -1,10 +1,47 @@
-import { Entity } from 'typeorm';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('products')
 export class Product {
     constructor(initialData: Partial<Product> = null) {
         if (initialData !== null) {
-            Object.assign(this, initialData)
+            Object.assign(this, initialData);
         }
     }
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ type: 'varchar' })
+    name: string;
+
+    @Column ({ name: 'unit_price', type: 'numeric' })
+    unitPrice: number;
+
+    @Column ({ type: 'text' })
+    description: string;
+
+    @Column ({ type: 'varchar' })
+    image: string;
+
+    @CreateDateColumn({
+        name: 'created_at',
+        type: 'timestamptz',
+        default: 'now()',
+        readonly: true,
+    })
+    createdAt: string;
+
+    @UpdateDateColumn({
+        name: 'updated_at',
+        type: 'timestamptz',
+        default: 'now()',
+    })
+
+    updatedAt: string;
+
+    @DeleteDateColumn ({
+        name: 'deleted_at',
+        type: 'timestamptz',
+    })
+    deletedAt: string;
 }
