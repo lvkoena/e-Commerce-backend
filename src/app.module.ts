14cc02bbd0623257fa82f6e83c9890a9d5058a6a
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-06-19 15:29:12 +0200

    created database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products/products.controller';
+// import { ProductsModule } from './modules/products/products.module';
+import { ProductsService } from './services/products/products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './modules/products/products.module';
-import { ProductService } from './services/products/products.service';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'putInUserNameHere',
+      password: 'putInPasswordHere',
+      database: 'yourDataBaseName',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
+  
   controllers: [AppController, ProductsController],
-  providers: [AppService, ProductService],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
